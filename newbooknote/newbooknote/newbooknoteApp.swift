33f945ead8f923fc3985c0f5bcf97ff46d75b6ed
Leo <brightspread.jo@gmail.com>
2024-01-27 15:08:57 +0900

    🎉 초기 프로젝트 코드 세팅

---
+++ newbooknote/newbooknote/newbooknoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  newbooknoteApp.swift
+//  newbooknote
+//
+//  Created by Leo on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct newbooknoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
